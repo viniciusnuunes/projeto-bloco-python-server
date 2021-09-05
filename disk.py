468c4bef389246b51b8c0b992d9b18a9d0b64983
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2021-09-05 15:58:59 -0300

    retornando informacoes necessarias

--- disk.py
+++ disk.py
@@ -4,4 +4,9 @@ import psutil
 def getDiskUsageInfo():
     disk = psutil.disk_usage('/')
 
+    disk = {'disk_total': disk[0],
+            'disk_use': disk[1],
+            'disk_free': disk[2],
+            'disk_percent': disk[3]}
+
     return disk
--- memory.py
+++ memory.py
@@ -4,4 +4,12 @@ import psutil
 def getMemoryInfo():
     memory = psutil.virtual_memory()
 
+    memory = {
+        'memory_total': memory[0],
+        'memory_use': memory[3],
+        'memory_free': memory[1],
+        'memory_percent': memory[2],
+        'memory_swap': psutil.swap_memory()
+    }
+
     return memory
