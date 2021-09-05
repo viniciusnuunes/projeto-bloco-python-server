dc3336affee35a54c38bd20a7b6e29f0e53e895c
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2021-09-05 16:21:09 -0300

    removendo chamada da funcao de pid

--- pid.py
+++ pid.py
@@ -18,17 +18,14 @@ def getPidInfo():
             'pid_date_creation': time.ctime(pid.create_time()),
             'pid_user_time': pid.cpu_times().user,
             'pid_system_time': pid.cpu_times().system,
-            'pid_system_percent': pid.cpu_percent(interval = 1.0),
+            'pid_system_percent': pid.cpu_percent(interval=1.0),
             'pid_memory_percent': pid.memory_percent(),
             'pid_memory_use': pid.memory_info().rss,
             'pid_threads': pid.num_threads()
-            
+
         }
     except Exception as error:
         print(f'Erro ao obter o PID: {str(error)}')
         pid = -1
 
     return pid
-
-
-print(getPidInfo())
